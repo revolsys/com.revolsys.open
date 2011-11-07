a4b9a7479922fc44b88b7fde53a9a67b9adf6f47
Paul Austin <paul.austin@revolsys.com>
2011-11-07 10:56:51 -0800

    Added new angle comparator

---
+++ com.revolsys.open.gis.core/src/main/java/com/revolsys/gis/model/coordinates/comparator/AngleFromPointComparator.java
@@ -0,0 +1,26 @@
+package com.revolsys.gis.model.coordinates.comparator;
+
+import java.util.Comparator;
+
+import com.revolsys.gis.model.coordinates.Coordinates;
+
+public class AngleFromPointComparator implements Comparator<Coordinates> {
+
+  private final Coordinates point;
+
+  public AngleFromPointComparator(final Coordinates point) {
+    this.point = point;
+  }
+
+  public int compare(final Coordinates c1, final Coordinates c2) {
+    final double angleC1 = point.angle2d(c1);
+    final double angleC2 = point.angle2d(c2);
+    if (angleC1 < angleC2) {
+      return 1;
+    } else if (angleC1 > angleC2) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }
+}
