eb37bf6c5d2e3ae37b29d6ed1856f9164f0c6f9c
Paul Austin <paul.austin@revolsys.com>
2011-11-07 11:24:31 -0800

    updated

--- release.sh
+++ release.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 if [[ "$1" == 201* ]]; then
   VERSION=$1.RELEASE
 else
@@ -7,16 +5,11 @@ else
 fi
 
 git pull
-git branch releases
-git checkout releases
-git merge master
 git tag -d $VERSION
 git push origin :$VERSION
 mvn release:clean
 mvn release:prepare -DautoVersionSubmodules=true -DdevelopmentVersion=TRUNK-SNAPSHOT -DreleaseVersion=${VERSION} -Dtag=${VERSION} -DupdateWorkingCopyVersions=false -DpreparationGoals=clean
 mvn release:perform
-git checkout master
-git branch -D releases
 git push origin
 git push origin :$VERSION
 git push origin $VERSION
