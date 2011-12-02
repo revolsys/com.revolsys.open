7ed40eeafbe210befe03eaf46f331b941f8172e4
Paul Austin <paul.austin@revolsys.com>
2011-12-02 14:12:40 -0800

    Updated site deployment

--- site.sh
+++ site.sh
@@ -1,17 +1,21 @@
 STATUS=`git status | grep "nothing to commit"`
 if [ -n "$STATUS" ]; then
-  mvn clean
-
   git pull
-  git checkout gh-pages
-  
-  for dir in `find . -type d -name site`; do
-    module=${dir/.\//}
-    module=${dir/\/target\/site\//}
-    echo $module
-  done
+  mvn clean site
 
-  git checkout master
+  git clone .git -b gh-pages target/gh-pages
+  
+  rm -rf target/gh-pages/*
+  for dir in `find . -type d -name site`; do
+    module=${dir//.\//}
+    module=${module//target\/site/}
+    module=${module//\//}
+    
+    rsync -a ${dir}/ target/gh-pages/${module}/
+  done
+  cd target/gh-pages
+  git commit -a -m "Site update"
+  git push
 else
   echo Checkin changes before creating site
 fi
