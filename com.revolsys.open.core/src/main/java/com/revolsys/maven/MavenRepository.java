ad80d358d5b5995f2488df3646a370e0c33ecd24
Paul Austin <paul.austin@revolsys.com>
2011-12-09 17:26:40 -0800

    Code to get pom and dependencies

--- com.revolsys.open.core/src/main/java/com/revolsys/maven/MavenRepository.java
+++ com.revolsys.open.core/src/main/java/com/revolsys/maven/MavenRepository.java
@@ -1,8 +1,13 @@
 package com.revolsys.maven;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.core.io.Resource;
 import org.springframework.util.StringUtils;
 
+import com.revolsys.io.xml.XmlMapIoFactory;
 import com.revolsys.spring.SpringUtil;
 
 public class MavenRepository {
@@ -12,6 +17,11 @@ public class MavenRepository {
   public MavenRepository() {
   }
 
+  /**
+   * Root resource must end in a /
+   * 
+   * @param root
+   */
   public MavenRepository(final Resource root) {
     this.root = root;
   }
@@ -33,9 +43,36 @@ public class MavenRepository {
     return artifactResource;
   }
 
+  public Map<String, Object> getPom(final String groupId,
+    final String artifactId, final String version) {
+    Resource resource = getResource(groupId, artifactId, version, "pom");
+    if (resource.exists()) {
+      return XmlMapIoFactory.toMap(resource);
+    } else {
+      throw new IllegalArgumentException("Pom does not exist for " + groupId
+        + ":" + artifactId + ":" + version + " at " + resource);
+    }
+  }
+
+  public List<Map<String, Object>> getDependencies(final String groupId,
+    final String artifactId, final String version) {
+    Map<String, Object> pom = getPom(groupId, artifactId, version);
+    Map<String, Object> dependencyMap = (Map<String, Object>)pom.get("dependencies");
+    if (dependencyMap == null) {
+      return Collections.emptyList();
+    } else {
+      List<Map<String, Object>> dependencyList = (List<Map<String, Object>>)dependencyMap.get("dependency");
+      if (dependencyList == null) {
+        return dependencyList;
+      } else {
+        return dependencyList;
+      }
+    }
+  }
+
   protected StringBuffer getPath(final String groupId, final String artifactId,
     final String version, final String classifier, final String type) {
-    final StringBuffer path = new StringBuffer('/');
+    final StringBuffer path = new StringBuffer();
     path.append('/');
     path.append(groupId.replace('.', '/'));
     path.append('/');
