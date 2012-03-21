f9c1b5e816f167394ded1071efa3c21b8980daba
Paul Austin <paul.austin@revolsys.com>
2012-03-21 13:25:12 -0700

    hasRoleRegEx

---
+++ com.revolsys.open.web/src/main/java/com/revolsys/spring/security/DefaultWebSecurityExpressionHandler.java
@@ -0,0 +1,52 @@
+package com.revolsys.spring.security;
+
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+import org.springframework.security.access.expression.SecurityExpressionRoot;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.authentication.AuthenticationTrustResolver;
+import org.springframework.security.authentication.AuthenticationTrustResolverImpl;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.FilterInvocation;
+import org.springframework.security.web.access.expression.WebSecurityExpressionHandler;
+
+/**
+ * Facade which isolates Spring Security's requirements for evaluating
+ * web-security expressions from the implementation of the underlying expression
+ * objects.
+ * 
+ * @author Luke Taylor
+ * @since 3.0
+ */
+public class DefaultWebSecurityExpressionHandler implements
+  WebSecurityExpressionHandler {
+
+  private AuthenticationTrustResolver trustResolver = new AuthenticationTrustResolverImpl();
+
+  private ExpressionParser expressionParser = new SpelExpressionParser();
+
+  private RoleHierarchy roleHierarchy;
+
+  public ExpressionParser getExpressionParser() {
+    return expressionParser;
+  }
+
+  public EvaluationContext createEvaluationContext(
+    Authentication authentication,
+    FilterInvocation fi) {
+    StandardEvaluationContext ctx = new StandardEvaluationContext();
+    SecurityExpressionRoot root = new WebSecurityExpressionRoot(authentication,
+      fi);
+    root.setTrustResolver(trustResolver);
+    root.setRoleHierarchy(roleHierarchy);
+    ctx.setRootObject(root);
+
+    return ctx;
+  }
+
+  public void setRoleHierarchy(RoleHierarchy roleHierarchy) {
+    this.roleHierarchy = roleHierarchy;
+  }
+}
--- com.revolsys.open.web/src/main/java/com/revolsys/spring/security/MethodSecurityExpressionRoot.java
+++ com.revolsys.open.web/src/main/java/com/revolsys/spring/security/MethodSecurityExpressionRoot.java
@@ -1,18 +1,6 @@
 package com.revolsys.spring.security;
 
-import java.io.Serializable;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.springframework.security.access.PermissionEvaluator;
-import org.springframework.security.access.expression.SecurityExpressionRoot;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
 
 /**
  * Extended expression root object which contains extra method-specific
@@ -22,8 +10,6 @@ import org.springframework.security.core.authority.AuthorityUtils;
  * @since 3.0
  */
 public class MethodSecurityExpressionRoot extends SecurityExpressionRoot {
-  private PermissionEvaluator permissionEvaluator;
-
   private Object filterObject;
 
   private Object returnObject;
@@ -38,27 +24,8 @@ public class MethodSecurityExpressionRoot extends SecurityExpressionRoot {
 
   public final String admin = "administration";
 
-  private RoleHierarchy roleHierarchy;
-
-  private Set<String> roles;
-
   public MethodSecurityExpressionRoot(final Authentication a) {
     super(a);
-  }
-
-  public Set<String> getAuthoritySet() {
-    if (roles == null) {
-      roles = new HashSet<String>();
-      Collection<GrantedAuthority> userAuthorities = authentication.getAuthorities();
-
-      if (getReturnObject() != null) {
-        userAuthorities = roleHierarchy.getReachableGrantedAuthorities(userAuthorities);
-      }
-
-      roles = AuthorityUtils.authorityListToSet(userAuthorities);
-    }
-
-    return roles;
   }
 
   public Object getFilterObject() {
@@ -69,47 +36,11 @@ public class MethodSecurityExpressionRoot extends SecurityExpressionRoot {
     return returnObject;
   }
 
-  public boolean hasPermission(final Object target, final Object permission) {
-    return permissionEvaluator.hasPermission(authentication, target, permission);
-  }
-
-  public boolean hasPermission(
-    final Object targetId,
-    final String targetType,
-    final Object permission) {
-    return permissionEvaluator.hasPermission(authentication,
-      (Serializable)targetId, targetType, permission);
-  }
-
-  public boolean hasRoleRegex(final String regex) {
-    final Pattern pattern = Pattern.compile(regex);
-    for (final String role : getAuthoritySet()) {
-      final Matcher matcher = pattern.matcher(role);
-      final boolean matches = matcher.matches();
-      if (matches) {
-        return true;
-      }
-    }
-    return false;
-  }
-
   public void setFilterObject(final Object filterObject) {
     this.filterObject = filterObject;
-  }
-
-  public void setPermissionEvaluator(
-    final PermissionEvaluator permissionEvaluator) {
-    this.permissionEvaluator = permissionEvaluator;
   }
 
   public void setReturnObject(final Object returnObject) {
     this.returnObject = returnObject;
   }
-
-  @Override
-  public void setRoleHierarchy(final RoleHierarchy roleHierarchy) {
-    super.setRoleHierarchy(roleHierarchy);
-    this.roleHierarchy = roleHierarchy;
-  }
-
 }
