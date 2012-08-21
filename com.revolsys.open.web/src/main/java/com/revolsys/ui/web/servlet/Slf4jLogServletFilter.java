dabcb436b911307ecceb103fd37b9f9483efeb9c
Paul Austin <paul.austin@revolsys.com>
2012-08-21 10:06:34 -0700

    x

--- com.revolsys.open.web/src/main/java/com/revolsys/ui/web/servlet/Slf4jLogServletFilter.java
+++ com.revolsys.open.web/src/main/java/com/revolsys/ui/web/servlet/Slf4jLogServletFilter.java
@@ -8,6 +8,7 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,10 +32,10 @@ public class Slf4jLogServletFilter implements Filter {
     } catch (final ServletException e) {
       throw e;
     } catch (final RuntimeException e) {
-      HttpServletLogUtil.logRequestException(log, null, e);
+      HttpServletLogUtil.logRequestException(log, (HttpServletRequest)request, e);
       throw e;
     } catch (final Error e) {
-      HttpServletLogUtil.logRequestException(log, null, e);
+      HttpServletLogUtil.logRequestException(log, (HttpServletRequest)request, e);
       throw e;
     }
   }
