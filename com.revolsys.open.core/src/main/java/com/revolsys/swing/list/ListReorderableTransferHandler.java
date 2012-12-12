f95a3e80bb2a15553159cf906d563f02009cf58f
Paul Austin <paul.austin@revolsys.com>
2012-12-12 11:06:16 -0800

    multi selection

--- com.revolsys.open.core/src/main/java/com/revolsys/swing/list/ListReorderableTransferHandler.java
+++ com.revolsys.open.core/src/main/java/com/revolsys/swing/list/ListReorderableTransferHandler.java
@@ -9,6 +9,7 @@ import javax.activation.DataHandler;
 import javax.swing.JComponent;
 import javax.swing.JList;
 import javax.swing.ListModel;
+import javax.swing.ListSelectionModel;
 import javax.swing.TransferHandler;
 
 import com.revolsys.util.Reorderable;
@@ -16,10 +17,10 @@ import com.revolsys.util.Reorderable;
 public class ListReorderableTransferHandler extends TransferHandler {
   private static final long serialVersionUID = 1L;
 
-  private final DataFlavor localObjectFlavor = new DataFlavor(Integer.class,
-    "Integer Row Index");
+  private final DataFlavor localObjectFlavor = new DataFlavor(int[].class,
+    "Integer[]");
 
- private  String mimeType = localObjectFlavor.getMimeType();
+  private String mimeType = localObjectFlavor.getMimeType();
 
   private JList list;
 
@@ -30,10 +31,9 @@ public class ListReorderableTransferHandler extends TransferHandler {
   @Override
   protected Transferable createTransferable(JComponent c) {
     assert (c == list);
-    int selectedRow = list.getSelectedIndex();
+    int[] selectedRows = list.getSelectedIndices();
     if (c == list) {
-
-      return new DataHandler(selectedRow, mimeType);
+      return new DataHandler(selectedRows, mimeType);
     } else {
       return null;
     }
@@ -62,22 +62,40 @@ public class ListReorderableTransferHandler extends TransferHandler {
   public boolean importData(TransferHandler.TransferSupport info) {
     JList target = (JList)info.getComponent();
     JList.DropLocation dropLocation = (JList.DropLocation)info.getDropLocation();
-    int index = dropLocation.getIndex();
+    int dropIndex = dropLocation.getIndex();
     ListModel model = list.getModel();
     int max = model.getSize();
-    if (index < 0 || index > max) {
-      index = max;
+    if (dropIndex < 0 || dropIndex > max) {
+      dropIndex = max;
     }
     target.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
     try {
       Transferable transferable = info.getTransferable();
-      Integer rowFrom = (Integer)transferable.getTransferData(localObjectFlavor);
-      if (rowFrom != -1 && rowFrom != index) {
-        ((Reorderable)model).reorder(rowFrom, index);
-        if (index > rowFrom) {
-          index--;
+      int[] indices = (int[])transferable.getTransferData(localObjectFlavor);
+      if (indices.length > 0) {
+        Reorderable reorderable = (Reorderable)model;
+        int currentIndex = dropIndex;
+        int count = 0;
+        for (int index : indices) {
+          if (count > 0) {
+            if (currentIndex > index) {
+              index -= count;
+            }
+          }
+          count++;
+          if (index == currentIndex) {
+            currentIndex++;
+          } else {
+            reorderable.reorder(index, currentIndex);
+            if (index > currentIndex) {
+              currentIndex++;
+            }
+          }
         }
-        target.getSelectionModel().addSelectionInterval(index, index);
+        ListSelectionModel selectionModel = target.getSelectionModel();
+        selectionModel.clearSelection();
+        selectionModel.addSelectionInterval(currentIndex - indices.length,
+          currentIndex - 1);
         return true;
       }
     } catch (Exception e) {
