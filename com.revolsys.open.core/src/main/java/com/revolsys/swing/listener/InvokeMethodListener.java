2277e056396bbe234939c9c07e190e7270adb7b4
Paul Austin <paul.austin@revolsys.com>
2012-12-14 13:19:31 -0800

    x

--- com.revolsys.open.core/src/main/java/com/revolsys/swing/listener/InvokeMethodListener.java
+++ com.revolsys.open.core/src/main/java/com/revolsys/swing/listener/InvokeMethodListener.java
@@ -6,32 +6,35 @@ import java.awt.event.ActionListener;
 import javax.swing.SwingUtilities;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import com.revolsys.parallel.process.InvokeMethodRunnable;
 
-public class InvokeMethodListener extends InvokeMethodRunnable implements ActionListener, DocumentListener {
- 
+public class InvokeMethodListener extends InvokeMethodRunnable implements
+  ActionListener, DocumentListener, ListSelectionListener {
+
   private final boolean invokeLater;
 
-  public InvokeMethodListener(final boolean invokeLater,
-    final Class<?> clazz, final String methodName, final Object... parameters) {
+  public InvokeMethodListener(final boolean invokeLater, final Class<?> clazz,
+    final String methodName, final Object... parameters) {
     super(clazz, methodName, parameters);
     this.invokeLater = invokeLater;
   }
 
-  public InvokeMethodListener(final boolean invokeLater,
-    final Object object, final String methodName, final Object... parameters) {
+  public InvokeMethodListener(final boolean invokeLater, final Object object,
+    final String methodName, final Object... parameters) {
     super(object, methodName, parameters);
     this.invokeLater = invokeLater;
   }
 
-  public InvokeMethodListener(final Class<?> clazz,
-    final String methodName, final Object... parameters) {
+  public InvokeMethodListener(final Class<?> clazz, final String methodName,
+    final Object... parameters) {
     this(true, clazz, methodName, parameters);
   }
 
-  public InvokeMethodListener(final Object object,
-    final String methodName, final Object... parameters) {
+  public InvokeMethodListener(final Object object, final String methodName,
+    final Object... parameters) {
     this(true, object, methodName, parameters);
   }
 
@@ -40,7 +43,6 @@ public class InvokeMethodListener extends InvokeMethodRunnable implements Action
     invokeMethod();
   }
 
-  
   protected void invokeMethod() {
     if (invokeLater) {
       SwingUtilities.invokeLater(this);
@@ -64,4 +66,8 @@ public class InvokeMethodListener extends InvokeMethodRunnable implements Action
     invokeMethod();
   }
 
+  @Override
+  public void valueChanged(ListSelectionEvent e) {
+    invokeMethod();
+  }
 }
