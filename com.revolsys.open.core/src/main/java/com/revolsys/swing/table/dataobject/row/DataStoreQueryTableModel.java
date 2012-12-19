693b5c8c3025b31afc29c0097f34b62d40492b90
Paul Austin <paul.austin@revolsys.com>
2012-12-19 13:34:23 -0800

    Changes to use SwingWorkerManager

--- com.revolsys.open.core/src/main/java/com/revolsys/swing/table/dataobject/row/DataStoreQueryTableModel.java
+++ com.revolsys.open.core/src/main/java/com/revolsys/swing/table/dataobject/row/DataStoreQueryTableModel.java
@@ -17,6 +17,7 @@ import com.revolsys.gis.data.io.DataObjectStore;
 import com.revolsys.gis.data.model.DataObject;
 import com.revolsys.gis.data.model.DataObjectMetaData;
 import com.revolsys.gis.data.query.Query;
+import com.revolsys.swing.SwingWorkerManager;
 import com.revolsys.swing.table.SortableTableModel;
 
 public class DataStoreQueryTableModel extends DataObjectRowTableModel implements
@@ -41,7 +42,7 @@ public class DataStoreQueryTableModel extends DataObjectRowTableModel implements
 
   ResultPager<DataObject> pager;
 
-  SwingWorker<Void, Void> pagerWorker;
+  SwingWorker<?, ?> pagerWorker;
 
   private Map<Integer, DataObject> cache = new LruMap<Integer, DataObject>(100);
 
@@ -72,14 +73,15 @@ public class DataStoreQueryTableModel extends DataObjectRowTableModel implements
 
   @Override
   public SortOrder setSortOrder(int column) {
-    synchronized (this) {
-      pager = null;
+    synchronized (cache) {
       if (pagerWorker != null) {
         pagerWorker.cancel(true);
         pagerWorker = null;
       }
     }
-    return super.setSortOrder(column);
+    SortOrder sortOrder = super.setSortOrder(column);
+    createPagerWorker();
+    return sortOrder;
   }
 
   public DataObject getObject(final int row) {
@@ -101,45 +103,44 @@ public class DataStoreQueryTableModel extends DataObjectRowTableModel implements
   }
 
   ResultPager<DataObject> getPager() {
-    synchronized (this) {
+    synchronized (cache) {
       if (pager == null) {
-        synchronized (this) {
-          if (pagerWorker == null) {
-            pagerWorker = new SwingWorker<Void, Void>() {
-              @Override
-              protected Void doInBackground() throws Exception {
-                DataObjectMetaData metaData = getMetaData();
-                final Query query = new Query(metaData);
-                for (Entry<Integer, SortOrder> entry : getSortedColumns().entrySet()) {
-                  Integer column = entry.getKey();
-                  String name = getAttributeName(column);
-                  SortOrder sortOrder = entry.getValue();
-                  if (sortOrder == SortOrder.ASCENDING) {
-                    query.addOrderBy(name, true);
-                  } else if (sortOrder == SortOrder.DESCENDING) {
-                    query.addOrderBy(name, false);
-                  }
-                }
-                pager = dataStore.page(query);
-                pager.setPageSize(1);
-                synchronized (cache) {
-                  cache.clear();
-                }
-                fireTableDataChanged();
-                pagerWorker = null;
-                return null;
-              }
-
-              @Override
-              protected void done() {
-              }
-            };
-            pagerWorker.execute();
-          }
+        if (pagerWorker == null) {
+          createPagerWorker();
         }
       }
       return pager;
     }
+  }
+
+  private void createPagerWorker() {
+    pagerWorker = SwingWorkerManager.execute("Load data", this, "loadData");
+  }
+
+  public void loadData() {
+    DataObjectMetaData metaData = getMetaData();
+    final Query query = new Query(metaData);
+    for (Entry<Integer, SortOrder> entry : getSortedColumns().entrySet()) {
+      Integer column = entry.getKey();
+      String name = getAttributeName(column);
+      SortOrder sortOrder = entry.getValue();
+      if (sortOrder == SortOrder.ASCENDING) {
+        query.addOrderBy(name, true);
+      } else if (sortOrder == SortOrder.DESCENDING) {
+        query.addOrderBy(name, false);
+      }
+    }
+    ResultPager<DataObject> pager = dataStore.page(query);
+    pager.setPageSize(1);
+    synchronized (cache) {
+      if (this.pager != null) {
+        this.pager.close();
+      }
+      this.pager = pager;
+      cache.clear();
+      pagerWorker = null;
+    }
+    fireTableDataChanged();
   }
 
 }
