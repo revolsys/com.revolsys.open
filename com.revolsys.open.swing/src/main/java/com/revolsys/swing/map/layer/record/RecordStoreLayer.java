31a334f768df8c957c42a94da7a8f4ec56c23f7c
Paul Austin <paul.austin@revolsys.com>
2015-01-06 16:16:19 -0800

    saving session deleted records

--- com.revolsys.open.swing/src/main/java/com/revolsys/swing/map/layer/record/RecordStoreLayer.java
+++ com.revolsys.open.swing/src/main/java/com/revolsys/swing/map/layer/record/RecordStoreLayer.java
@@ -313,8 +313,8 @@ public class RecordStoreLayer extends AbstractRecordLayer {
       final Map<String, String> connectionProperties = getProperty("connection");
       if (connectionProperties == null) {
         LoggerFactory.getLogger(getClass())
-        .error(
-          "A record store layer requires a connectionProperties entry with a name or url, username, and password: "
+          .error(
+            "A record store layer requires a connectionProperties entry with a name or url, username, and password: "
               + getPath());
         return false;
       } else {
@@ -402,7 +402,7 @@ public class RecordStoreLayer extends AbstractRecordLayer {
           final Statistics statistics = query.getProperty("statistics");
           query.setProperty("recordFactory", this);
           try (
-              final Reader<LayerRecord> reader = (Reader)recordStore.query(query)) {
+            final Reader<LayerRecord> reader = (Reader)recordStore.query(query)) {
             final List<LayerRecord> records = new ArrayList<>();
             for (final LayerRecord record : reader) {
               final boolean added = addProxyRecordToList(records, record);
@@ -432,7 +432,7 @@ public class RecordStoreLayer extends AbstractRecordLayer {
       synchronized (getSync()) {
         final BoundingBox loadBoundingBox = boundingBox.expandPercent(0.2);
         if (!this.loadedBoundingBox.covers(boundingBox)
-            && !this.loadingBoundingBox.covers(boundingBox)) {
+          && !this.loadingBoundingBox.covers(boundingBox)) {
           if (this.loadingWorker != null) {
             this.loadingWorker.cancel(true);
           }
@@ -479,26 +479,26 @@ public class RecordStoreLayer extends AbstractRecordLayer {
 
   @Override
   protected boolean doSaveChanges(final LayerRecord record) {
-    final boolean deleted = isDeleted(record);
+    final boolean deleted = super.isDeleted(record);
     final RecordStore recordStore = getRecordStore();
     final PlatformTransactionManager transactionManager = recordStore.getTransactionManager();
     try (
-        Transaction transaction = new Transaction(transactionManager,
-          Propagation.REQUIRES_NEW)) {
+      Transaction transaction = new Transaction(transactionManager,
+        Propagation.REQUIRES_NEW)) {
       try {
 
         if (isExists()) {
           if (recordStore != null) {
             try (
-                final Writer<Record> writer = recordStore.createWriter()) {
+              final Writer<Record> writer = recordStore.createWriter()) {
               if (isCached(this.getCacheIdDeleted(), record)
-                  || super.isDeleted(record)) {
+                || super.isDeleted(record)) {
                 preDeleteRecord(record);
                 record.setState(RecordState.Deleted);
                 writer.write(record);
               } else if (super.isModified(record)) {
                 writer.write(record);
-              } else if (isNew(record)) {
+              } else if (super.isNew(record)) {
                 Identifier identifier = record.getIdentifier();
                 final RecordDefinition recordDefinition = getRecordDefinition();
                 final List<String> idFieldNames = recordDefinition.getIdFieldNames();
@@ -820,7 +820,7 @@ public class RecordStoreLayer extends AbstractRecordLayer {
       boolean removed = false;
       if (isLayerRecord(record)) {
         for (final Label cacheId : new ArrayList<>(
-            this.cacheIdToRecordIdMap.keySet())) {
+          this.cacheIdToRecordIdMap.keySet())) {
           removed |= removeRecordFromCache(cacheId, record);
         }
       }
