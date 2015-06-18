dfdca1cc900c8110a2ea58ade2e37d9b9351c212
Paul Austin <paul.austin@revolsys.com>
2015-06-18 15:48:19 -0700

    window opener

---
+++ com.revolsys.open.swing/src/main/java/com/revolsys/swing/component/WindowOpener.java
@@ -0,0 +1,100 @@
+package com.revolsys.swing.component;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Window;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.LayoutStyle;
+import javax.swing.WindowConstants;
+
+import org.jdesktop.swingx.JXBusyLabel;
+
+import com.revolsys.awt.WebColors;
+import com.revolsys.swing.SwingUtil;
+import com.revolsys.swing.layout.GroupLayoutUtil;
+import com.revolsys.swing.parallel.Invoke;
+
+public class WindowOpener extends JFrame implements WindowListener {
+
+  /**
+   *
+   */
+  private static final long serialVersionUID = 1L;
+
+  public static WindowOpener create(final Window window, final String title, final String message) {
+    return new WindowOpener(window, title, message);
+  }
+
+  private WindowOpener(final Window window, final String title, final String message) {
+    super(title);
+    setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+    if (window instanceof JFrame) {
+      final JFrame frame = (JFrame)window;
+      frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+    }
+    setBackground(WebColors.White);
+
+    final JPanel panel = new JPanel(new BorderLayout());
+    setContentPane(panel);
+    panel.setBorder(BorderFactory.createEmptyBorder(10, 5, 10, 5));
+    final JXBusyLabel busyLabel = new JXBusyLabel(new Dimension(16, 16));
+    busyLabel.setBusy(true);
+    panel.add(busyLabel);
+    panel.add(new JLabel(message));
+
+    GroupLayoutUtil.makeColumns(LayoutStyle.getInstance(), panel, 2);
+    setMinimumSize(new Dimension(title.length() * 15 + 40, 70));
+    pack();
+    SwingUtil.setLocationCentre(this);
+    setVisible(true);
+    window.setVisible(false);
+  }
+
+  public void addOpeningWindow(final Window window) {
+    Invoke.later(() -> {
+      if (window.isVisible()) {
+        setVisible(false);
+        window.removeWindowListener(this);
+      } else {
+        window.addWindowListener(this);
+      }
+    });
+  }
+
+  @Override
+  public void windowActivated(final WindowEvent e) {
+  }
+
+  @Override
+  public void windowClosed(final WindowEvent e) {
+  }
+
+  @Override
+  public void windowClosing(final WindowEvent e) {
+  }
+
+  @Override
+  public void windowDeactivated(final WindowEvent e) {
+  }
+
+  @Override
+  public void windowDeiconified(final WindowEvent e) {
+  }
+
+  @Override
+  public void windowIconified(final WindowEvent e) {
+  }
+
+  @Override
+  public void windowOpened(final WindowEvent e) {
+    final Window window = e.getWindow();
+    window.removeWindowListener(this);
+    setVisible(false);
+  }
+}
