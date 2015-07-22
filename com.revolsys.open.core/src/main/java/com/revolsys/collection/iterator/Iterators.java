35b5d0cd2d75344fdd8f6f535fd233f24d324cd8
Paul Austin <paul.austin@revolsys.com>
2015-07-22 14:28:03 -0700

    x

---
+++ com.revolsys.open.core/src/main/java/com/revolsys/collection/iterator/Iterators.java
@@ -0,0 +1,23 @@
+package com.revolsys.collection.iterator;
+
+import java.util.Iterator;
+import java.util.function.Predicate;
+
+public class Iterators {
+  public static <V> Iterable<V> filter(final Iterable<V> iterable, final Predicate<V> filter) {
+    if (filter == null) {
+      return iterable;
+    } else {
+      final Iterator<V> iterator = iterable.iterator();
+      return new PredicateIterator<>(filter, iterator);
+    }
+  }
+
+  public static <V> Iterator<V> filter(final Iterator<V> iterator, final Predicate<V> filter) {
+    if (filter == null) {
+      return iterator;
+    } else {
+      return new PredicateIterator<>(filter, iterator);
+    }
+  }
+}
