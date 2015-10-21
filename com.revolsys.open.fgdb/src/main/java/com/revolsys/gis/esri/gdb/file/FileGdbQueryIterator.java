47290c422ab7a6203a690c845275a1b9602e2e4e
Paul Austin <paul.austin@revolsys.com>
2015-10-21 08:40:47 -0700

    Avoid NOE issues on FGDB writer close

--- com.revolsys.open.fgdb/src/main/java/com/revolsys/gis/esri/gdb/file/FileGdbQueryIterator.java
+++ com.revolsys.open.fgdb/src/main/java/com/revolsys/gis/esri/gdb/file/FileGdbQueryIterator.java
@@ -33,7 +33,7 @@ public class FileGdbQueryIterator extends AbstractIterator<Record> {
 
   private int limit = Integer.MAX_VALUE;
 
-  private final int offset;
+  private int offset;
 
   private RecordDefinition recordDefinition;
 
@@ -48,6 +48,8 @@ public class FileGdbQueryIterator extends AbstractIterator<Record> {
   private Statistics statistics;
 
   private Table table;
+
+  private boolean closed = false;
 
   FileGdbQueryIterator(final FileGdbRecordStore recordStore, final String catalogPath) {
     this(recordStore, catalogPath, "*", "", null, 0, -1);
@@ -71,51 +73,65 @@ public class FileGdbQueryIterator extends AbstractIterator<Record> {
   FileGdbQueryIterator(final FileGdbRecordStore recordStore, final String catalogPath,
     final String fields, final String sql, final BoundingBox boundingBox, final int offset,
     final int limit) {
-    this.recordStore = recordStore;
     this.catalogPath = catalogPath;
-    this.recordDefinition = recordStore.getRecordDefinition(catalogPath);
-    this.table = recordStore.getTable(catalogPath);
-    if (this.recordDefinition != null && "*".equals(fields)) {
-      this.fields = Strings.toString(this.recordDefinition.getFieldNames());
-    } else {
-      this.fields = fields;
-    }
     this.sql = sql;
-    setBoundingBox(boundingBox);
-    this.recordFactory = recordStore.getRecordFactory();
-    this.offset = offset;
-    if (limit >= 0) {
-      this.limit = limit;
-    }
-  }
-
-  @Override
-  protected void doClose() {
-    if (this.recordStore != null) {
-      try {
-        try {
-          this.recordStore.closeEnumRows(this.rows);
-        } finally {
-          this.recordStore.releaseTable(this.catalogPath);
-        }
-      } catch (final Throwable e) {
-        Exceptions.log(getClass(), "Error closing query: " + this.catalogPath, e);
-      } finally {
-        this.boundingBox = null;
-        this.recordStore = null;
-        this.fields = null;
-        this.recordDefinition = null;
-        this.rows = null;
-        this.sql = null;
-        this.table = null;
+    this.recordDefinition = recordStore.getRecordDefinition(catalogPath);
+    if (this.recordDefinition == null) {
+      this.closed = true;
+    } else {
+      this.recordStore = recordStore;
+      this.table = recordStore.getTable(catalogPath);
+      if ("*".equals(fields)) {
+        this.fields = Strings.toString(this.recordDefinition.getFieldNames());
+      } else {
+        this.fields = fields;
+      }
+      setBoundingBox(boundingBox);
+      this.recordFactory = recordStore.getRecordFactory();
+      this.offset = offset;
+      if (limit >= 0) {
+        this.limit = limit;
       }
     }
   }
 
   @Override
-  protected void doInit() {
-    if (this.recordDefinition != null) {
-      synchronized (this.recordStore) {
+  protected void doClose() {
+    boolean close = true;
+    if (this.closed || this.recordStore == null) {
+      close = false;
+    } else {
+      this.closed = true;
+    }
+    if (close) {
+      synchronized (this) {
+        if (this.recordDefinition != null) {
+          this.recordDefinition = null;
+          try {
+            try {
+              this.recordStore.closeEnumRows(this.rows);
+            } finally {
+              this.recordStore.releaseTable(this.catalogPath);
+            }
+          } catch (final Throwable e) {
+            Exceptions.log(getClass(), "Error closing query: " + this.catalogPath, e);
+          } finally {
+            this.boundingBox = null;
+            this.recordStore = null;
+            this.fields = null;
+            this.rows = null;
+            this.sql = null;
+            this.table = null;
+          }
+        }
+      }
+    }
+  }
+
+  @Override
+  protected synchronized void doInit() {
+    if (!this.closed) {
+      synchronized (this.recordStore.getApiSync()) {
         if (this.boundingBox == null) {
           if (this.sql.startsWith("SELECT")) {
             this.rows = this.recordStore.query(this.sql, true);
@@ -145,24 +161,29 @@ public class FileGdbQueryIterator extends AbstractIterator<Record> {
   }
 
   @Override
-  protected Record getNext() throws NoSuchElementException {
-    if (this.rows == null || this.recordDefinition == null) {
+  protected synchronized Record getNext() throws NoSuchElementException {
+    final FileGdbRecordStore recordStore = this.recordStore;
+    final EnumRows rows = this.rows;
+    if (rows == null || this.closed) {
       throw new NoSuchElementException();
     } else {
       Row row = null;
       while (this.offset > 0 && this.count < this.offset) {
-        row = this.recordStore.nextRow(this.rows);
+        row = recordStore.nextRow(rows);
         if (row == null) {
           throw new NoSuchElementException();
         } else {
-          this.recordStore.closeRow(row);
+          recordStore.closeRow(row);
         }
         this.count++;
+        if (this.closed) {
+          throw new NoSuchElementException();
+        }
       }
       if (this.count - this.offset >= this.limit) {
         throw new NoSuchElementException();
       }
-      row = this.recordStore.nextRow(this.rows);
+      row = recordStore.nextRow(rows);
       this.count++;
       if (row == null) {
         throw new NoSuchElementException();
@@ -170,7 +191,7 @@ public class FileGdbQueryIterator extends AbstractIterator<Record> {
         try {
           final Record record = this.recordFactory.newRecord(this.recordDefinition);
           if (this.statistics == null) {
-            this.recordStore.addStatistic("query", record);
+            recordStore.addStatistic("query", record);
           } else {
             this.statistics.add(record);
           }
@@ -178,17 +199,19 @@ public class FileGdbQueryIterator extends AbstractIterator<Record> {
           for (final FieldDefinition field : this.recordDefinition.getFields()) {
             final String name = field.getName();
             final AbstractFileGdbFieldDefinition esriFieldDefinition = (AbstractFileGdbFieldDefinition)field;
-            final Object value;
-            synchronized (this.recordStore) {
-              value = esriFieldDefinition.getValue(row);
-            }
+            final Object value = esriFieldDefinition.getValue(row);
             record.setValue(name, value);
+            if (this.closed) {
+              throw new NoSuchElementException();
+            }
           }
           record.setState(RecordState.Persisted);
+          if (this.closed) {
+            throw new NoSuchElementException();
+          }
           return record;
-
         } finally {
-          this.recordStore.closeRow(row);
+          recordStore.closeRow(row);
         }
       }
     }
