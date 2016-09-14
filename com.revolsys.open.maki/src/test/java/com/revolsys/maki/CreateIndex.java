245695676f92f1478ae1b99add3bfa37bb948d95
Paul Austin <paul.austin@revolsys.com>
2016-09-14 09:00:00 -0700

    x

--- com.revolsys.open.maki/src/test/java/com/revolsys/maki/CreateIndex.java
+++ com.revolsys.open.maki/src/test/java/com/revolsys/maki/CreateIndex.java
@@ -28,6 +28,5 @@ public class CreateIndex {
     MapObjectFactory.write(symbolLibraryFile, symbolLibrary);
 
     final SymbolLibrary symbolLibrary2 = MapObjectFactory.toObject(symbolLibraryFile);
-    System.out.println(symbolLibrary2);
   }
 }
