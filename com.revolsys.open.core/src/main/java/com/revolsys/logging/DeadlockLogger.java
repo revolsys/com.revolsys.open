ce3040a37461d848575a6693961f17ce0cc87c85
Paul Austin <paul.austin@revolsys.com>
2017-01-04 14:05:08 -0800

    Deadlock full stack trace

--- com.revolsys.open.core/src/main/java/com/revolsys/logging/DeadlockLogger.java
+++ com.revolsys.open.core/src/main/java/com/revolsys/logging/DeadlockLogger.java
@@ -1,6 +1,9 @@
 package com.revolsys.logging;
 
+import java.lang.Thread.State;
+import java.lang.management.LockInfo;
 import java.lang.management.ManagementFactory;
+import java.lang.management.MonitorInfo;
 import java.lang.management.ThreadInfo;
 import java.lang.management.ThreadMXBean;
 
@@ -39,8 +42,8 @@ public class DeadlockLogger implements Runnable {
           for (int i = 0; i < threadInfos1.length; i++) {
             final ThreadInfo threadInfo1 = threadInfos1[i];
             final ThreadInfo threadInfo2 = threadInfos2[i];
-            message.append(threadInfo1);
-            message.append(threadInfo2);
+            message.append(toString(threadInfo1));
+            message.append(toString(threadInfo2));
           }
           Logs.error(this, message.toString());
           // Quit thread now point running if there is a deadlock
@@ -49,5 +52,79 @@ public class DeadlockLogger implements Runnable {
 
       }
     }
+  }
+
+  private Object toString(final ThreadInfo threadInfo) {
+    final String threadName = threadInfo.getThreadName();
+    final long threadId = threadInfo.getThreadId();
+    final State threadState = threadInfo.getThreadState();
+    final StringBuilder sb = new StringBuilder(
+      "\"" + threadName + "\"" + " Id=" + threadId + " " + threadState);
+    final String lockName = threadInfo.getLockName();
+    if (lockName != null) {
+      sb.append(" on " + lockName);
+    }
+    final String lockOwnerName = threadInfo.getLockOwnerName();
+    if (lockOwnerName != null) {
+      final long lockOwnerId = threadInfo.getLockOwnerId();
+      sb.append(" owned by \"" + lockOwnerName + "\" Id=" + lockOwnerId);
+    }
+    if (threadInfo.isSuspended()) {
+      sb.append(" (suspended)");
+    }
+    if (threadInfo.isInNative()) {
+      sb.append(" (in native)");
+    }
+    sb.append('\n');
+    final StackTraceElement[] stackTrace = threadInfo.getStackTrace();
+    int i = 0;
+    for (; i < stackTrace.length && i < 500; i++) {
+      final StackTraceElement ste = stackTrace[i];
+      sb.append("\tat " + ste.toString());
+      sb.append('\n');
+      final LockInfo lockInfo = threadInfo.getLockInfo();
+      if (i == 0 && lockInfo != null) {
+        final Thread.State ts = threadState;
+        switch (ts) {
+          case BLOCKED:
+            sb.append("\t-  blocked on " + lockInfo);
+            sb.append('\n');
+          break;
+          case WAITING:
+            sb.append("\t-  waiting on " + lockInfo);
+            sb.append('\n');
+          break;
+          case TIMED_WAITING:
+            sb.append("\t-  waiting on " + lockInfo);
+            sb.append('\n');
+          break;
+          default:
+        }
+      }
+
+      final MonitorInfo[] lockedMonitors = threadInfo.getLockedMonitors();
+      for (final MonitorInfo mi : lockedMonitors) {
+        if (mi.getLockedStackDepth() == i) {
+          sb.append("\t-  locked " + mi);
+          sb.append('\n');
+        }
+      }
+    }
+    if (i < stackTrace.length) {
+      sb.append("\t...");
+      sb.append('\n');
+    }
+
+    final LockInfo[] locks = threadInfo.getLockedSynchronizers();
+    if (locks.length > 0) {
+      sb.append("\n\tNumber of locked synchronizers = " + locks.length);
+      sb.append('\n');
+      for (final LockInfo li : locks) {
+        sb.append("\t- " + li);
+        sb.append('\n');
+      }
+    }
+    sb.append('\n');
+    return sb.toString();
   }
 }
