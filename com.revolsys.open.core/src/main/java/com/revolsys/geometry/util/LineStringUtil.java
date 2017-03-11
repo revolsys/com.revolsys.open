eae580be51b9f0a5b67f8d8fd068a2c0085188c3
Paul Austin <paul.austin@revolsys.com>
2017-03-11 15:04:35 -0800

    Add method

--- com.revolsys.open.core/src/main/java/com/revolsys/geometry/util/LineStringUtil.java
+++ com.revolsys.open.core/src/main/java/com/revolsys/geometry/util/LineStringUtil.java
@@ -232,6 +232,15 @@ public final class LineStringUtil {
     return z;
   }
 
+  @Deprecated
+  public static Point getEndPoint(final LineString line, final boolean fromPoint) {
+    if (fromPoint) {
+      return line.getPoint(0);
+    } else {
+      return line.getPoint(-1);
+    }
+  }
+
   public static boolean hasEqualExact2d(final List<LineString> lines, final LineString newLine) {
     for (final LineString line : lines) {
       if (line.equals(2, newLine)) {
