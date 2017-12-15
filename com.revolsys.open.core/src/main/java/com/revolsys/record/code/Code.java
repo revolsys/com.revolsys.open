a2086bd9c5b8e5cf03c2cc1a95cf75ec6029890b
Paul Austin <paul.austin@revolsys.com>
2017-12-15 15:07:05 -0800

    Code interface enhancements

--- com.revolsys.open.core/src/main/java/com/revolsys/record/code/Code.java
+++ com.revolsys.open.core/src/main/java/com/revolsys/record/code/Code.java
@@ -1,12 +1,34 @@
 package com.revolsys.record.code;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.revolsys.identifier.Identifier;
 import com.revolsys.util.Describable;
 
 public interface Code extends Describable, Identifier {
+
+  static <C extends Code> List<String> descriptions(final Class<C> enumClass) {
+    final List<String> descriptions = new ArrayList<>();
+    for (final C code : enumClass.getEnumConstants()) {
+      final String description = code.getDescription();
+      descriptions.add(description);
+    }
+    return descriptions;
+  }
+
+  static List<String> descriptions(final List<? extends Code> codes) {
+    final List<String> descriptions = new ArrayList<>();
+    for (final Code code : codes) {
+      final String description = code.getDescription();
+      descriptions.add(description);
+    }
+    return descriptions;
+  }
+
   static String getCode(final Object value) {
     if (value == null) {
       return null;
@@ -16,6 +38,20 @@ public interface Code extends Describable, Identifier {
     } else {
       return value.toString();
     }
+  }
+
+  static <C extends Code> Map<String, C> getEnumCodeMap(final Class<C> enumClass) {
+    final Map<String, C> code = new HashMap<>();
+    for (final C enumValue : enumClass.getEnumConstants()) {
+      final String codeId = enumValue.getCode();
+      code.put(codeId, enumValue);
+    }
+    return Collections.unmodifiableMap(code);
+  }
+
+  default boolean equalsCode(final String code) {
+    final String codeThis = getCode();
+    return codeThis.equals(code);
   }
 
   String getCode();
