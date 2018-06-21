5b2ecf0882cc979d4dd9c9eba3dc4e29804d3501
paul <paul@PAUSTIN-WIN10>
2018-06-21 15:14:58 -0700

    isreadable

--- com.revolsys.open.core/src/main/java/com/revolsys/elevation/gridded/GriddedElevationModelReader.java
+++ com.revolsys.open.core/src/main/java/com/revolsys/elevation/gridded/GriddedElevationModelReader.java
@@ -2,10 +2,15 @@ package com.revolsys.elevation.gridded;
 
 import com.revolsys.geometry.model.BoundingBoxProxy;
 import com.revolsys.io.BaseCloseable;
+import com.revolsys.io.IoFactory;
 import com.revolsys.properties.ObjectWithProperties;
 
 public interface GriddedElevationModelReader
   extends BaseCloseable, BoundingBoxProxy, ObjectWithProperties {
+  static boolean isReadable(final Object source) {
+    return IoFactory.isAvailable(GriddedElevationModelReadFactory.class, source);
+  }
+
   @Override
   default void close() {
     ObjectWithProperties.super.close();
