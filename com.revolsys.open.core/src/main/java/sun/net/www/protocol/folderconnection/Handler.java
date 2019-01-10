c5fa4c71a6dc0613f85357c3192a7e315d035f2c
Paul Austin <paul.austin@revolsys.com>
2019-01-09 21:07:40 -0800

    version cleanup

--- com.revolsys.open.core/src/main/java/sun/net/www/protocol/folderconnection/Handler.java
+++
@@ -1,6 +0,0 @@
-package sun.net.www.protocol.folderconnection;
-
-import com.revolsys.net.protocol.folderconnection.FolderConnectionURLStreamHandler;
-
-public class Handler extends FolderConnectionURLStreamHandler {
-}
