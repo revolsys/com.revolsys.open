cca07d40011e8834b7696c05da258efeaee38e53
Paul Austin <paul.austin@revolsys.com>
2019-01-14 17:39:50 -0800

    fgdb sync with master

--- com.revolsys.open.fgdb/src/test/java/com/revolsys/gis/esri/gdb/file/test/field/XmlField.java
+++
@@ -1,9 +0,0 @@
-package com.revolsys.gis.esri.gdb.file.test.field;
-
-public class XmlField extends StringField {
-
-  public XmlField(final String name, final int length, final boolean required) {
-    super(name, length, required);
-  }
-
-}
