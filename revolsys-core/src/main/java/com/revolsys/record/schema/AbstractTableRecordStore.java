d01046eb2ea162b1a71ea332ea66516604fbad52
Paul Austin <paul.austin@automutatio.com>
2022-01-31 10:01:29 -0800

    pom

--- revolsys-core/src/main/java/com/revolsys/record/schema/AbstractTableRecordStore.java
+++ revolsys-core/src/main/java/com/revolsys/record/schema/AbstractTableRecordStore.java
@@ -390,8 +390,10 @@ public class AbstractTableRecordStore {
     }
   }
 
-  public Condition newODataFilter(final String filter) {
+  public Condition newODataFilter(String filter) {
     if (Property.hasValue(filter)) {
+      filter = filter.replace("%2B", "+");
+
       final TableReference table = getTable();
       return (Condition)ODataParser.parseFilter(table, filter);
     } else {
