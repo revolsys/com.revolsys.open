c0891bfa62c1f579b95bd6eb475319239545aebd
Paul Austin <paul.austin@automutatio.com>
2022-02-21 06:52:12 -0800

    Removed Debug

--- revolsys-core/src/main/java/com/revolsys/http/AzureManagedIdentityBearerToken.java
+++ revolsys-core/src/main/java/com/revolsys/http/AzureManagedIdentityBearerToken.java
@@ -12,9 +12,6 @@ public class AzureManagedIdentityBearerToken extends BearerToken {
     setExpireTime(expireTime);
     final String returnedResource = config.getString("resource");
     setScope(resource, returnedResource);
-    System.out.println("------------------------");
-    System.out.println(config);
-    System.out.println("------------------------");
   }
 
 }
